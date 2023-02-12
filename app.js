c5dd76e1658f48ae711e9b4ee6e1765aed1be718
Strezex <124489820+Strezex@users.noreply.github.com>
2023-02-12 10:40:05 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+function changeColor() {
+    let r = randomNumber(0,255);
+    let g = randomNumber(0,255);
+    let b = randomNumber(0,255);
+
+    document.body.style.background = `rgb(${r}, ${g}, ${b})`;
+}
+
+function randomNumber(min, max) {
+    return Math.floor((Math.random() * (max - min)) + min);
+}
+function changeColorWhite() {
+    document.body.style.background = 'rgb(255,255,255)';
+}
